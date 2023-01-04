38155f59ede557e803f2e0abce05c634bc858de5
Sieroxmania <102622965+Sieroxmania@users.noreply.github.com>
2023-01-04 14:13:52 +0100

    Jenkins bulding

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"}]
+packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"}];updateSearchResults();
--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"HelloWorld"}]
+typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"HelloWorld"}];updateSearchResults();
